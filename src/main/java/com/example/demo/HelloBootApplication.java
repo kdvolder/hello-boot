87dd0e608c10b04f52ef843cd033238b27222a87
Kris De Volder <kdevolder@pivotal.io>
2019-05-29 12:02:34 -0700

    Junk

--- src/main/java/com/example/demo/HelloBootApplication.java
+++ src/main/java/com/example/demo/HelloBootApplication.java
@@ -4,16 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
 @RestController
 public class HelloBootApplication {
 
+	@GetMapping("/bye")
+	public String bye(@RequestParam String param) {
+		return "So long";
+	}
+
 	@GetMapping(value = "/")
 	public String hello() {
 		return "Hello World!";
 	}
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(HelloBootApplication.class, args);
